29c9991786a9cb8cae129e43d2016aaa243dda5a
Madeleine Udell <madeleine.udell@gmail.com>
2014-02-12 10:33:14 -0800

    Initial commit

---
+++ src/indexing.jl
@@ -0,0 +1,63 @@
+#getindex(A::SharedSparseMatrixCSC, i::Integer) = getindex(A, ind2sub(size(A),i))
+getindex(A::SharedSparseMatrixCSC, I::(Integer,Integer)) = getindex(A, I[1], I[2])
+
+function getindex{T}(A::SharedSparseMatrixCSC{T}, i0::Integer, i1::Integer)
+    if !(1 <= i0 <= A.m && 1 <= i1 <= A.n); throw(BoundsError()); end
+    first = A.colptr[i1]
+    last = A.colptr[i1+1]-1
+    while first <= last
+        mid = (first + last) >> 1
+        t = A.rowval[mid]
+        if t == i0
+            return A.nzval[mid]
+        elseif t > i0
+            last = mid - 1
+        else
+            first = mid + 1
+        end
+    end
+    return zero(T)
+end
+
+getindex{T<:Integer}(A::SharedSparseMatrixCSC, I::AbstractVector{T}, j::Integer) = getindex(A,I,[j])
+getindex{T<:Integer}(A::SharedSparseMatrixCSC, i::Integer, J::AbstractVector{T}) = getindex(A,[i],J)
+
+### Indexing sets of columns and rows --- not yet fully implemented. One template example is below
+
+function getindex_cols{Tv,Ti}(A::SharedSparseMatrixCSC{Tv,Ti}, J::AbstractVector)
+
+    (m, n) = size(A)
+    nJ = length(J)
+
+    colptrA = A.colptr; rowvalA = A.rowval; nzvalA = A.nzval
+
+    colptrS = SharedArray(Ti, nJ+1;pids=A.pids)
+    colptrS[1] = 1
+    nnzS = 0
+
+    for j = 1:nJ
+        col = J[j]
+        nnzS += colptrA[col+1] - colptrA[col]
+        colptrS[j+1] = nnzS + 1
+    end
+
+    rowvalS = SharedArray(Ti, nnzS;pids=A.pids)
+    nzvalS  = SharedArray(Tv, nnzS;pids=A.pids)
+    ptrS = 0
+
+    for j = 1:nJ
+        col = J[j]
+
+        for k = colptrA[col]:colptrA[col+1]-1
+            ptrS += 1
+            rowvalS[ptrS] = rowvalA[k]
+            nzvalS[ptrS] = nzvalA[k]
+        end
+    end
+
+    return SharedSparseMatrix(m, nJ, colptrS, rowvalS, nzvalS, A.pids)
+
+end
+
+## setindex! not yet implemented, because can't splice a shared array
+
---
+++ test/test.jl
@@ -0,0 +1,44 @@
+using Base.Test
+addprocs(1)
+# @everywhere include("../src/parallel_matmul.jl")
+@everywhere using ParallelSparseMatMul
+
+### test matrix multiplication
+
+m = 100;  n = 10; p = .1
+A = shsprand(m,n,p)
+L = operator(A);
+x = Base.shmem_rand(n);
+y = Base.shmem_rand(m);
+x_out = copy(x)
+y_out = copy(y)
+
+S = localize(A);
+# these don't work b/c output is 0 even when assignment is correct
+y_out_loc = S*x
+x_out_loc = A'*y
+
+y_out = A*x
+@test y_out_loc == y_out
+x_out = A'*y
+@test x_out_loc == x_out
+y_out = L*x
+@test y_out_loc == y_out
+x_out = L'*y
+@test x_out_loc == x_out
+
+@test y_out_loc == A*x
+@test x_out_loc == A'*y
+@test y_out_loc == L*x
+@test x_out_loc == L'*y
+
+### test matrix creation and localization
+@test localize(share(S)) == S
+
+Aeye = shspeye(Float64,m,n)
+@test localize(Aeye) == speye(Float64,m,n)
+
+### test indexing
+i = min(n,m) - 3
+@test A[i,i] == 1 
+@test A[i,j] == 0
